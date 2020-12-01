ee781d1583dbfd089d24b0721b1356f51247feab
inman <sf990668852@gmail.com>
2020-12-01 13:46:03 +0800

    add a lot of stars

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
-import React from 'react'
+import React, {useMemo , Suspense} from 'react'
 import Box from './components/Box'
 import './styles.css'
-import { Canvas } from 'react-three-fiber'
+
+import * as THREE from 'three'
+import { Canvas, useLoader, useFrame } from 'react-three-fiber'
+
+import smokeImg from './img/smoke.png'
+import starsImg from './img/stars.jpg'
+
+
+
+function Stars({ count = 50 }) {
+  
+  const [ smoke123 , stars123 ] = useLoader(THREE.TextureLoader, [ smokeImg, starsImg ])
+  
+  const positions = useMemo(() => {
+    let positions = []
+    for (let i = 0; i < count; i++) {
+      positions.push(Math.random() * 10 * (Math.round(Math.random()) ? -40 : 40))//x
+      positions.push(0)//y
+      positions.push(Math.random()*500 - 500 )//z
+    }
+    return new Float32Array(positions)
+  }, [count])
+  
+  return (
+    <points>
+      <bufferGeometry attach="geometry">
+        <bufferAttribute
+          attachObject={['attributes', 'position']}
+          count={positions.length / 3}
+          array={positions}
+          itemSize={3}
+        />
+      </bufferGeometry>
+      <pointsMaterial attach="material" size={1} sizeAttenuation color="white" transparent opacity={0.8} />
+    </points>
+  )
+}
 
 const App =()=>{
   
   return(
 
     <Canvas>
-    
+      <Suspense fallback={null}>
+        <Stars/>
+      </Suspense>
+      
       <ambientLight 
         color={'#555555'} 
         intensity={1}
