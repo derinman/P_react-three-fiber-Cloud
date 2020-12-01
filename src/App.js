aa004dcec057ae0ba0b9bb9a74656779f186ea87
inman <sf990668852@gmail.com>
2020-12-01 11:53:12 +0800

    add some

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState } from 'react'
+import React from 'react'
 import Box from './components/Box'
 import './styles.css'
 import { Canvas } from 'react-three-fiber'
@@ -6,16 +6,52 @@ import { Canvas } from 'react-three-fiber'
 const App =()=>{
   
   return(
-    <div>
+
     <Canvas>
-    <ambientLight />
-    <pointLight position={[10, 10, 10]} />
-    <Box position={[-1.2, 1.8, 0]}/>
-    <Box position={[1.2, 1.8, 0]}/>
-    <Box position={[-1.2, -1.8, 0]}/>
-    <Box position={[1.2, -1.8, 0]}/>
+    
+      <ambientLight 
+        color={'#555555'} 
+        intensity={1}
+      />
+      <directionalLight 
+        color={'#ff8c19'} 
+        intensity={1}
+        position={[0, 0, 1]} 
+      />
+    
+      <pointLight 
+        color={'#cc6600'}  
+        intensity={50}
+        distance={450}
+        decay={1.7}
+        position={[200, 300, 100]} 
+      />
+      <pointLight 
+        color={'#d8547e'}  
+        intensity={50}
+        distance={450}
+        decay={1.7}
+        position={[100, 300, 100]} 
+      />
+      <pointLight 
+        color={'#3677ac'}  
+        intensity={50}
+        distance={450}
+        decay={1.7}
+        position={[300, 300, 200]} 
+      />
+
+      {/*color,near,far*/}
+      <fog attach="fog" args={['#03544e', 5, -5]}  />
+      
+      <Box position={[-1.2, 1.8, 0]}/>
+      <Box position={[1.2, 1.8, 0]}/>
+      <Box position={[-1.2, -1.8, 0]}/>
+      <Box position={[1.2, -1.8, 0]}/>
+
+
     </Canvas>
-    </div>
+
   )
 }
 
--- src/components/Box.js
+++ src/components/Box.js
@@ -4,9 +4,9 @@ import { useFrame } from 'react-three-fiber'
 const Box = (props) => {
     // This reference will give us direct access to the mesh
     const mesh = useRef()
-    
+
     // Set up state for the hovered and active state
-    const [hovered, setHover] = useState(false)
+    const [hover, setHover] = useState(false)
     const [active, setActive] = useState(false)
     
     // Rotate mesh every frame, this is outside of React without overhead
@@ -21,7 +21,7 @@ const Box = (props) => {
         onPointerOver={(e) => setHover(true)}
         onPointerOut={(e) => setHover(false)}>
         <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-        <meshStandardMaterial attach="material" color={hovered ? 'hotpink' : 'orange'} />
+        <meshStandardMaterial attach="material" color={'#FFFFFF'} />
         </mesh>
     );
 }
